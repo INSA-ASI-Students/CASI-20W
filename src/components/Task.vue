45b17f8515b4077f8738925225ad22fe5b600a5e
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-06 22:10:25 +0100

    HOTFIX - display task description

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -4,7 +4,7 @@
       <div class="card-title h5 text-gray">{{self.title}}</div>
       <div class="card-subtitle text-gray">{{self.information}}</div>
     </div>
-    <div class="card-body">{{self.description}}</div>
+    <div class="card-body text-primary">{{self.description}}</div>
     <div class="card-footer">
       <button class="btn btn-primary">Done</button>
     </div>
