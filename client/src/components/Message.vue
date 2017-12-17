ff4c8592a73949d0b7fe23f75247c90eb7faf62c
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-17 18:40:55 +0100

    UPDATE - avatar

--- client/src/components/Message.vue
+++ client/src/components/Message.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="">
-    {{self.content}}
+  <div class="tile bg-primary">
+    <div class="tile-icon">
+      <avatar v-bind:self="self.user"/>
+    </div>
+    <div class="tile-content">
+      <p class="tile-title">{{self.user.name}}</p>
+      <p class="tile-subtitle text-gray">{{self.content}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import store from '../store';
+import Avatar from './Avatar.vue';
 
 export default {
   name: 'message',
@@ -14,8 +21,12 @@ export default {
     'self',
   ],
   methods: {
+  },
+  components: {
+    Avatar,
   }
 }
 </script>
 <style lang="scss" scoped>
+
 </style>
