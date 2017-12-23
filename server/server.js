7b3a5c5e9a34aeeaf68695cecd24304a8b87a5ac
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-23 14:26:21 +0100

    UPDATE - add winston lib for logs

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 /* eslint no-console:"off", no-underscore-dangle:"off" */
 const express = require('express');
 const bodyParser = require('body-parser');
+const winston = require('winston');
 
 const taskListRessource = require('./ressources/TaskListRessource');
 const taskRessource = require('./ressources/TaskRessource');
 const userRessource = require('./ressources/UserRessource');
 
+if (process.env.LOG_LEVEL) winston.level = process.env.LOG_LEVEL;
+else winston.level = 'debug';
+
 const app = express();
+const host = 'localhost';
+const port = 8080;
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
@@ -17,4 +23,10 @@ taskListRessource(app, './database/taskLists.db');
 taskRessource(app, './database/tasks.db');
 userRessource(app, './database/users.db');
 
-app.listen(8080);
+const server = app.listen(port, host, () => {
+  winston.log('info', 'Server listening', {
+    date: new Date(),
+    address: server.address().address,
+    port: server.address().port,
+  });
+});
