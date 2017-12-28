e186c1b6c3270eead2f8184f9f91232bf63f4b5b
Nistof <nistof.contact@gmail.com>
2017-12-28 14:37:09 +0100

    UPDATE - Sign (In/Up) components (Base)

---
+++ client/src/components/login/SignIn.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+import store from '../../store';
+
+export default {
+  name: 'sign-in',
+  store
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/src/components/login/SignUp.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+import store from '../../store';
+
+export default {
+  name: 'sign-up',
+  store
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
