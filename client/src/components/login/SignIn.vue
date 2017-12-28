de6ae36cb160c8cd96c1ad6998e1138db7bcf0ff
Nistof <nistof.contact@gmail.com>
2017-12-28 15:19:38 +0100

    UPDATE - Sign (In/Up) forms

--- client/src/components/login/SignIn.vue
+++ client/src/components/login/SignIn.vue
@@ -1,5 +1,21 @@
 <template>
-  
+  <div class="sign-in">
+    <div class="form-group">
+      <label class="form-label" for="input-username">Username</label>
+      <input class="form-input" type="text" id="input-username" placeholder="Username" v-model="username">
+      <label class="form-label" for="input-password">Password</label>
+      <input class="form-input" type="password" id="input-password" placeholder="Password" v-model="password">
+      <div class="btn-sign-in">
+        <button
+          class="btn btn-primary"
+          v-on:click="connect"
+          v-bind:disabled="username === '' || password === ''"
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,10 +23,34 @@ import store from '../../store';
 
 export default {
   name: 'sign-in',
-  store
+  store,
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    connect() {
+      // TODO Ask server for connection
+      this.$store.commit('switchPage', { page: 'board' }) // To do on success
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .sign-in {
+    padding: 7.5pt;
 
+    .form-group {
+      .btn-sign-in {
+        padding-top: 25pt;
+
+        .btn {
+          width: 100%;
+        }
+      }
+    }
+  }
 </style>
--- client/src/components/login/SignUp.vue
+++ client/src/components/login/SignUp.vue
@@ -1,5 +1,32 @@
 <template>
-  
+  <div class="sign-up">
+    <div class="form-group">
+      <label class="form-label" for="input-username">Username</label>
+      <input class="form-input" type="text" id="input-username" placeholder="Username" v-model="username">
+      <label class="form-label" for="input-password">Password</label>
+      <input
+        class="form-input"
+        v-bind:class="{ 'is-success': samePassword, 'is-error': !emptyPassword && !emptyConfirmPassword && !samePassword }"
+        type="password"
+        id="input-password"
+        placeholder="Password"
+        v-model="password"
+      >
+      <label class="form-label" for="input-confirm-password">Confirm password</label>
+      <input
+        class="form-input"
+        v-bind:class="{ 'is-success': samePassword, 'is-error': !emptyConfirmPassword && !samePassword }"
+        type="password"
+        id="input-confirm-password"
+        placeholder="Password"
+        v-model="confirmPassword"
+      >
+      <p class="form-input-hint" v-if="!emptyConfirmPassword && !samePassword">Passwords are different</p>
+      <div class="btn-sign-up">
+        <button class="btn btn-primary" v-bind:disabled="!samePassword">Sign Up</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,10 +34,44 @@ import store from '../../store';
 
 export default {
   name: 'sign-up',
-  store
+  store,
+  data() {
+    return {
+      username: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    emptyPassword() {
+      return this.password === '';
+    },
+    emptyConfirmPassword() {
+      return this.confirmPassword === '';
+    },
+    samePassword () {
+      return (
+        this.password !== '' && 
+        this.confirmPassword !== '' &&
+        this.password === this.confirmPassword
+      )
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .sign-up {
+    padding: 7.5pt;
 
+    .form-group {
+      .btn-sign-up {
+        padding-top: 25pt;
+
+        .btn {
+          width: 100%;
+        }
+      }
+    }
+  }
 </style>
