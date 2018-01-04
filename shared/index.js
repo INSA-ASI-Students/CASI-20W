99dcb76e0a6cbc728e8277bfc230c52d789082f0
Nistof <nistof.contact@gmail.com>
2018-01-04 10:31:38 +0100

    UPDATE - Shared index

--- shared/index.js
+++ shared/index.js
@@ -1,9 +1,11 @@
+const Board = require('./objects/Board');
 const Task = require('./objects/Task');
 const TaskList = require('./objects/TaskList');
 const User = require('./objects/User');
 const Message = require('./objects/Message');
 
 module.exports = {
+  Board,
   Task,
   TaskList,
   User,
