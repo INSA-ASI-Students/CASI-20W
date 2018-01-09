012399ba49eccfc23f91c98aa66e475a6187b957
Nistof <nistof.contact@gmail.com>
2018-01-09 14:13:14 +0100

    HOTFIX - Notify working (Problems with tasks)

--- client/src/ressources/receive.js
+++ client/src/ressources/receive.js
@@ -1,44 +1,36 @@
+/* global XMLHttpRequest */
 import Axios from 'axios';
 import config from '../../../shared/config.json';
 
 const updateRessource = (store, method, endpoint, id = undefined) => {
   switch (endpoint) {
-    case config.server.ressources.messages:
-      if (method.toLowerCase() === 'get' && id) {
-        store.commit('retrieveMessages');
-      }
+    case config.server.ressources.message.endpoint:
+      store.commit('retrieveMessages');
       break;
-    case config.server.ressources.boards:
+    case config.server.ressources.board.endpoint:
       break;
-    case config.server.ressources.tasks:
-      if (method.toLowerCase() === 'get' && id) {
-        store.commit('retrieveTaskList');
-      }
+    case config.server.ressources.task.endpoint:
+      store.commit('retrieveTaskList');
       break;
-    case config.server.ressources.taskLists:
-      if (method.toLowerCase() === 'get' && id) {
-        store.commit('retrieveTaskListGroup');
-      }
+    case config.server.ressources.taskList.endpoint:
+      store.commit('retrieveTaskListGroup');
       break;
-    case config.server.ressources.users:
-      if (method.toLowerCase() === 'get' && id) {
-        store.commit('retrieveUsers');
-      }
+    case config.server.ressources.user.endpoint:
+      store.commit('retrieveUsers');
       break;
     default:
   }
 };
 
 const getNotified = (store) => {
-  Axios({
-    method: 'get',
-    url: `http://${config.server.hostname}:${config.server.port}${config.server.ressources.notify.endpoint}`,
-  })
-    .then((res) => {
-      updateRessource(store, res.data.method, res.data.endpoint, res.data.id);
-      getNotified(store);
-    })
-    .catch(() => getNotified(store));
+  const xdr = new XMLHttpRequest();
+  xdr.onload = () => {
+    const res = JSON.parse(xdr.responseText);
+    updateRessource(store, res.method, res.endpoint, res.id);
+    getNotified(store);
+  };
+  xdr.open('GET', `http://${config.server.hostname}:${config.server.port}${config.server.ressources.notify.endpoint}`);
+  xdr.send();
 };
 
 export default store => getNotified(store);
